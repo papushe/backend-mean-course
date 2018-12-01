404b6da96e56b35aba146e2b9bbb5d23b855d731
Papushe <papushe9@gmail.com>
2018-12-01 14:29:23 +0200

    appjs

--- middleware/file.js
+++ middleware/file.js
@@ -1,25 +1,31 @@
 const multer = require('multer');
 
 const MIME_TYPE_MAP = {
-  "image/png": "png",
-  "image/jpeg": "jpg",
-  "image/jpg": "jpg",
+    "image/png": "png",
+    "image/jpeg": "jpg",
+    "image/jpg": "jpg",
 };
 
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    const isValid = MIME_TYPE_MAP[file.mimetype];
-    let error = new Error("Invalid mime type");
-    if (isValid) {
-      error = null
+    destination: (req, file, cb) => {
+        let fs = require('fs');
+        let dir = './images';
+
+        if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir);
+        }
+        const isValid = MIME_TYPE_MAP[file.mimetype];
+        let error = new Error("Invalid mime type");
+        if (isValid) {
+            error = null
+        }
+        cb(error, "./images");
+    },
+    filename: (req, file, cb) => {
+        const name = file.originalname.toLowerCase().split(' ').join('-');
+        const ext = MIME_TYPE_MAP[file.mimetype];
+        cb(null, name + '-' + Date.now() + '.' + ext);
     }
-    cb(error, "./images");
-  },
-  filename: (req, file, cb) => {
-    const name = file.originalname.toLowerCase().split(' ').join('-');
-    const ext = MIME_TYPE_MAP[file.mimetype];
-    cb(null, name + '-' + Date.now() + '.' + ext);
-  }
 
 });
 
