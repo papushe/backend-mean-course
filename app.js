72b63277922f344f0ed0be5e36f0c21347ccb973
Papushe <papushe9@gmail.com>
2018-12-01 14:12:01 +0200

    appjs

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ mongoose.connect("mongodb+srv://pap:" + process.env.MONGO_ATLAS_PW + "@cluster0-
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-app.use("/images", express.static(path.join("backend/images")));
+app.use("/images", express.static(path.join("./images")));
 
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
