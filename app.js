e8a93057af1fe22e76aab9ac6e1422c2a4510916
Papushe <papushe9@gmail.com>
2018-12-01 14:10:28 +0200

    appjs

--- app.js
+++ app.js
@@ -29,9 +29,7 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use('', (req, res) => {
-    res.sendFile(`${__dirname}/index.html`);
-});
+
 app.use('/api/posts', postsRoutes);
 app.use('/api/user', userRoutes);
 
